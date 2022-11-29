b1a261a9ca5ff8abdcd9289b2c5fba459a015b3c
Alexander S <akselsv@mail.ru>
2022-11-29 13:18:49 +0400

    Added functionality for more than 100 urls to work in semi-automatic mode. With comments and arrayoverflow catcher

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ const jwtClient = new google.auth.JWT(
   null
 );
 
-const batch = fs
-  .readFileSync('urls.txt')
-  .toString()
-  .split('\n');
+// collect all urls
+const batchAll = fs
+    .readFileSync('urls.txt')
+    .toString()
+    .split('\n');
+
+// set first url index in current pack to send
+var batchIndexFirst = parseInt(fs.readFileSync('Index.txt'));
+// catcher for end of mission
+if (batchAll.length < batchIndexFirst){
+  console.error(' Seems all URLs passed. Current Index is bigger, than sizeof batchAll. Setup 0 in Index.txt file ');
+  return;
+}
+// set last url index in current pack to send (limitation per operation is 100, per day 24h? 200)
+var batchIndexLast = batchIndexFirst + 100;
+// save last url index in file, that will be first index in next pack
+fs.writeFileSync('Index.txt', batchIndexLast.toString());
+// set batch variable with selected indexes
+var batch = batchAll.slice(batchIndexFirst, batchIndexLast);
+
+// sum of sent urls
+var resultCounter = 0;
 
 jwtClient.authorize(function(err, tokens) {
   if (err) {
@@ -47,5 +65,8 @@ jwtClient.authorize(function(err, tokens) {
   };
   request(options, (err, resp, body) => {
     console.log(body);
+    resultCounter++;
   });
 });
+// End report
+console.info('Task Done. URLs sent: ',resultCounter);
